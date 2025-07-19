1959d5253a1ab8b79989347b62d5ef2aebb80baa
DESTROYER <Rajaryan231207@gmail.com>
2025-07-20 03:11:44 +0530

    Update __main__.py

--- RAUSHAN/__main__.py
+++ RAUSHAN/__main__.py
@@ -31,13 +31,13 @@ filters.command("start")
 )
 async def start_command(client, message: Message):
   await message.reply_photo(
-                            photo = f"https://files.catbox.moe/id5nqm.jpg",
-                            caption = f"ʜᴇʏ, ᴛʜɪs ɪs ᴀ | ғᴀᴅᴇᴅ🕊ʙᴀɴᴀʟʟ🕊ʙᴏᴛ | ᴡʜɪᴄʜ ɪs ʙᴀsᴇᴅ ᴏɴ ᴘʏʀᴏɢʀᴀᴍ ʟɪʙʀᴀʀʏ ᴛᴏ ʙᴀɴ ᴏʀ ᴅᴇsᴛʀᴏʏ ᴀʟʟ ᴛʜᴇ ᴍᴇᴍʙᴇʀs ғʀᴏᴍ ᴀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ɪɴ ᴀ ғᴇᴡ sᴇᴄᴏɴᴅs ᴀɴᴅ ᴍᴀᴋᴇ ᴛʜᴇᴍ ғᴇᴇʟ ᴛʜᴇ ᴘᴏᴡᴇʀ ᴏғ @CRY4DED_FR 🐉!\n\nᴛᴏ ᴄʜᴇᴄᴋ ᴍʏ ᴀʙɪʟɪᴛʏ ɢɪᴠᴇ ᴍᴇ ғᴜʟʟ ᴘᴏᴡᴇʀs\n\nᴛʏᴘᴇ /ʙᴀɴᴀʟʟ ᴛᴏ ꜱᴇᴇ ᴍᴀɢɪᴄ ɪɴ ɢʀᴏᴜᴘ 🌚.",
+                            photo = f"https://files.catbox.moe/h1xa2a.jpg",
+                            caption = f"ʜᴇʏ, ᴛʜɪs ɪs ᴀ | ғᴀᴅᴇᴅ🕊ʙᴀɴᴀʟʟ🕊ʙᴏᴛ | ᴡʜɪᴄʜ ɪs ʙᴀsᴇᴅ ᴏɴ ᴘʏʀᴏɢʀᴀᴍ ʟɪʙʀᴀʀʏ ᴛᴏ ʙᴀɴ ᴏʀ ᴅᴇsᴛʀᴏʏ 💀,ᴀʟʟ ᴛʜᴇ ᴍᴇᴍʙᴇʀs ғʀᴏᴍ ᴀ ɢʀᴏᴜᴘ ᴡɪᴛʜɪɴ  ғᴇᴡ sᴇᴄᴏɴᴅs ᴀɴᴅ ᴍᴀᴋᴇ ᴛʜᴇᴍ ғᴇᴇʟ ᴛʜᴇ ᴘᴏᴡᴇʀ ᴏғ @CRY4DED_FR 🐉!\n\nᴛᴏ ᴄʜᴇᴄᴋ ᴍʏ ᴀʙɪʟɪᴛʏ ɢɪᴠᴇ ᴍᴇ ғᴜʟʟ ᴘᴏᴡᴇʀs\n\nᴛʏᴘᴇ /ʙᴀɴᴀʟʟ ᴛᴏ ꜱᴇᴇ ᴍᴀɢɪᴄ ɪɴ ɢʀᴏᴜᴘ 🌚.",
   reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "⚡owner🕊", url=f"https://t.me/CRY4DED_FR")
+                        "⦿ owner ⦿ ", url=f"https://t.me/CRY4DED_FR")
                 ]       
            ]
       )
