81a4e130426cffb7f1932537845f1bb529da4b4e
DESTROYER <Rajaryan231207@gmail.com>
2025-11-13 11:50:00 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,12 +2,12 @@ import os
 from flask import Flask
 from flask_restful import Resource, Api
 
-app = Flask(__name__)
-api = Api(app)
+web_app = Flask(__name__)
 
-class Greeting (Resource):
-    def get(self):
-        return "Faded Ban All Bot is Up & Running!"
+@web_app.route('/')
+def hello_world():
+    return 'Bot is alive!'
 
-api.add_resource(Greeting, '/')
-app.run(host="0.0.0.0", port=os.environ.get("PORT", 800))
+def run_web_server():
+    port = int(os.environ.get("PORT", 8080))
+    web_app.run(host='0.0.0.0', port=port)
